b59acf9de2798d9a80599256ff1fa35eed2d431b
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2015-05-14 19:47:39 -0300

    Changed user model: added get and set properties

--- ch4/app/models/user.server.model.js
+++ ch4/app/models/user.server.model.js
@@ -1,10 +1,59 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
+
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
-	username: String,
-	password: String
+	email: {
+		type: String,
+		index: true
+	},
+	username: {
+		type: String,
+		trim: true,
+		unique: true
+	},
+	password: String,
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	website: {
+		type: String,
+		set: function(url) {
+			if (!url) {
+				return url;
+			} else {
+				if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+					url = 'http://' + url;
+				}
+				return url;
+			}
+		},
+		get: function(url) {
+			if (!url) {
+				return url;
+			} else {
+				if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+					url = 'http://' + url;
+				}
+				return url;
+			}
+		}
+	}
 });
+
+UserSchema.virtual('fullName').get(function() {
+	return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+	var splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', {
+	getters: true,
+	virtuals: true
+});
+
 mongoose.model('User', UserSchema);
