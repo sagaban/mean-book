95fb0a58f117b50070436f9840558302be98f77a
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2015-05-25 18:41:11 -0300

    Added chapter 10 (copying ch9)

---
+++ ch10/config/env/development.fake.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: 'mongodb://localhost/mean-book',
+    sessionSecret: 'developmentSessionSecret',
+    facebook: {
+        clientID: 'Application Id',
+        clientSecret: 'Application Secret',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    }
+};
